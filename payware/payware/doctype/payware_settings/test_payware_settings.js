b12433a55cb8480b01d0aec83c330c0361039e43
Info Aakvatech <info@aakvatech.com>
2019-03-16 13:37:21 +0000

    feat: Updated payware settings and added column. Removed unnecessary paywarelist doctype

--- payware/payware/doctype/payware_settings/test_payware_settings.js
+++ payware/payware/doctype/payware_settings/test_payware_settings.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: PaywareList", function (assert) {
+QUnit.test("test: Payware Settings", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new PaywareList
-		() => frappe.tests.make('PaywareList', [
+		// insert a new Payware Settings
+		() => frappe.tests.make('Payware Settings', [
 			// values to be set
 			{key: 'value'}
 		]),
