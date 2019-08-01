405c2d0db6988571915cee5903ec4ca3f685de17
Info Aakvatech <info@aakvatech.com>
2019-08-01 08:28:11 +0000

    fix: Added number to words in report

--- payware/payware/utils.py
+++ payware/payware/utils.py
@@ -7,6 +7,7 @@ from frappe.utils import cint, format_datetime,get_datetime_str,now_datetime,add
 from frappe import _
 from erpnext.accounts.utils import get_fiscal_year
 from erpnext.hr.doctype.employee.employee import get_holiday_list_for_employee
+from num2words import num2words
 
 @frappe.whitelist()
 def create_disbursement_journal_entry(doc, method):
@@ -435,3 +436,95 @@ def generate_additional_salary_records():
 				additional_salary.save(ignore_permissions = True)
 				frappe.set_value("Additional Salary", additional_salary_doc.name, "last_transaction_date", next_date)
 				frappe.msgprint("New additional salary created for " + additional_salary_doc.auto_repeat_frequency + " dated " + str(next_date))
+
+@frappe.whitelist()
+def get_number_format_info(format):
+	number_format_info = {
+		"#,###.##": (".", ",", 2),
+		"#.###,##": (",", ".", 2),
+		"# ###.##": (".", " ", 2),
+		"# ###,##": (",", " ", 2),
+		"#'###.##": (".", "'", 2),
+		"#, ###.##": (".", ", ", 2),
+		"#,##,###.##": (".", ",", 2),
+		"#,###.###": (".", ",", 3),
+		"#.###": ("", ".", 0),
+		"#,###": ("", ",", 0)
+	}
+	return number_format_info.get(format) or (".", ",", 2)
+
+#
+# convert number to words
+#
+@frappe.whitelist()
+def in_words(integer, in_million=True):
+	"""
+	Returns string in words for the given integer.
+	"""
+	locale = 'en_IN' if not in_million else frappe.local.lang
+	integer = int(integer)
+	try:
+		ret = num2words(integer, lang=locale)
+	except NotImplementedError:
+		ret = num2words(integer, lang='en')
+	except OverflowError:
+		ret = num2words(integer, lang='en')
+	return ret.replace('-', ' ')
+
+#
+# convert currency to words
+#
+@frappe.whitelist()
+def money_in_words(number, main_currency = None, fraction_currency=None):
+	"""
+	Returns string in words with currency and fraction currency.
+	"""
+	from frappe.utils import get_defaults
+	_ = frappe._
+
+	try:
+		# note: `flt` returns 0 for invalid input and we don't want that
+		number = float(number)
+	except ValueError:
+		return ""
+
+	number = flt(number)
+	if number < 0:
+		return ""
+
+	d = get_defaults()
+	if not main_currency:
+		main_currency = d.get('currency', 'INR')
+	if not fraction_currency:
+		fraction_currency = frappe.db.get_value("Currency", main_currency, "fraction", cache=True) or _("Cent")
+
+	number_format = frappe.db.get_value("Currency", main_currency, "number_format", cache=True) or \
+		frappe.db.get_default("number_format") or "#,###.##"
+
+	fraction_length = get_number_format_info(number_format)[2]
+
+	n = "%.{0}f".format(fraction_length) % number
+
+	numbers = n.split('.')
+	main, fraction =  numbers if len(numbers) > 1 else [n, '00']
+
+	if len(fraction) < fraction_length:
+		zeros = '0' * (fraction_length - len(fraction))
+		fraction += zeros
+
+	in_million = True
+	if number_format == "#,##,###.##": in_million = False
+
+	# 0.00
+	if main == '0' and fraction in ['00', '000']:
+		out = "{0} {1}".format(main_currency, _('Zero'))
+	# 0.XX
+	elif main == '0':
+		out = _(in_words(fraction, in_million).title()) + ' ' + fraction_currency
+	else:
+		out = main_currency + ' ' + _(in_words(main, in_million).title())
+		if cint(fraction):
+			out = out + ' ' + _('and') + ' ' + _(in_words(fraction, in_million).title()) + ' ' + fraction_currency
+
+	return out + ' ' + _('only.')
+
