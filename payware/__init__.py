3c5d29efe0359dfc69a75c0b8fb2647fe9af8b60
Info Aakvatech <info@aakvatech.com>
2020-01-10 04:10:49 +0000

    feat: ZKTeco biotime integration

--- payware/__init__.py
+++ payware/__init__.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-<<<<<<< HEAD
 __version__ = '1.2.08'
-=======
-__version__ = '1.2.07'
->>>>>>> 761c0c1281db3acceee9bdf6fb5f913ce154cd7b
 
