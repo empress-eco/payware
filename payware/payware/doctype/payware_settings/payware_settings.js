e61ecc09cdecbcb5ddc336c84b186a494ebb2cc6
Info Aakvatech <info@aakvatech.com>
2020-03-06 10:02:37 +0000

    Feat Report ITX 300 PAYE SDL

--- payware/payware/doctype/payware_settings/payware_settings.js
+++ payware/payware/doctype/payware_settings/payware_settings.js
@@ -2,7 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Payware Settings', {
-	refresh: function(frm) {
-
+	setup: function(frm) {
+        frm.set_query('bank_account_for_tra', function() {
+            return {
+                filters: {
+                    
+                }
+            }
+        });
 	}
-});
+})
